fa135b4c5ac0d1b2ab05eb52e4987177c5d1f8b2
ArthourNychta <nox.regis@outlook.com>
2022-08-29 16:29:49 +0700

    push new feature

--- src/web.ts
+++ src/web.ts
@@ -3,6 +3,9 @@ import { WebPlugin } from '@capacitor/core';
 import type { MockDetectorPlugin } from './definitions';
 
 export class MockDetectorWeb extends WebPlugin implements MockDetectorPlugin {
+  detectMock(): Promise<any> {
+    throw new Error('Method not implemented.');
+  }
   async echo(options: { value: string }): Promise<{ value: string }> {
     console.log('ECHO', options);
     return options;
